45ce6937056d82e2df6e3ed82b5515c6b59e601d
kirari1020 <benkira1020@gmail.com>
2021-06-04 04:38:25 +0000

    devise導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+   before_action :configure_permitted_parameters, if: :devise_controller?  #ユーザ登録、ログイン認証などが使われる前に実行
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])   #ユーザ登録(sign_up)の際にユーザ名(name)のデータ操作を許可
+  end
 end
